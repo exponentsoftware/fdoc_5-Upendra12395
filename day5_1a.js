06dee83eb0ce50314ae2f11d61782535699edf8d
Upendra12395 <ch95upendra@gmail.com>
2021-10-22 19:14:00 +0530

    final submission

--- day5_1a.js
+++ day5_1a.js
@@ -4,15 +4,16 @@ const API_URL = 'https://restcountries.com/v3/all'
 const totalLanguage = async ()=>{
     const res = await axios(API_URL);
     let result = res.data
-    let language = [], languagesArray = []
-    // creating array of object of languages country wise
+    let languagesArray = [], language = []
+    //creating array of object of languages country wise
     for(let i=0; i<result.length;i++){
         language.push(result[i].languages)
     }
-    //console.log(language)
+    // let language = Array.from(result, ({languages}) => languages)
+     //console.log(language)
 //creating array of total language used
     for(let i=0; i<language.length;i++){
-        if (i == 157){              //used to skip, there is NULL value
+        if (i == 147){              //used to skip, there is NULL value
             continue
         }
         let keyLength = Object.keys(language[i]).length        
